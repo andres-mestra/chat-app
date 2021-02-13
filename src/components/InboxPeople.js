1fb1146f7dc2c03952de680815849509257494cc
andresdev <andresmestra11a@gmail.com>
2021-02-13 10:51:01 -0500

    Componentes  del sidebar para mostrar lista de chats

---
+++ src/components/InboxPeople.js
@@ -0,0 +1,12 @@
+import { SearchBox } from "./SearchBox"
+import { Sidebar } from "./Sidebar"
+
+
+export const InboxPeople = () => {
+  return (
+    <div className="inbox_people">
+      <SearchBox />
+      <Sidebar />
+    </div>
+  )
+}
