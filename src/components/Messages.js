b113c4cbd836185fa918629bcb5f106632c66459
andresdev <andresmestra11a@gmail.com>
2021-02-13 11:08:14 -0500

    Componentes para mostrar mensajes

---
+++ src/components/Messages.js
@@ -0,0 +1,20 @@
+import { IncomingMessage } from "./IncomingMessage"
+import { OutgoingMessage } from "./OutgoingMessage"
+import { SendMessage } from "./SendMessage"
+
+
+export const Messages = () => {
+  return (
+    <div className="mesgs">
+
+      {/* <!-- Historia inicio --> */}
+      <div className="msg_history">
+        <IncomingMessage/>
+        <OutgoingMessage />
+      </div>
+      {/* <!-- Historia Fin --> */}
+      <SendMessage />
+      
+    </div>
+  )
+}
