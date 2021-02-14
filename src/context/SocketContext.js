e953e38779cd57c3a5ca19f1be0a0dab801a5973
andresdev <andresmestra11a@gmail.com>
2021-02-13 20:56:29 -0500

    Integración socket.io

---
+++ src/context/SocketContext.js
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+import { useSocket } from '../hooks/useSocket'
+
+export const SocketContext = createContext();
+
+
+export const SocketProvider = ({ children }) => {
+
+    const { socket, online } = useSocket(process.env.REACT_APP_API_URL);
+    
+    return (
+        <SocketContext.Provider value={{ socket, online }}>
+            { children }
+        </SocketContext.Provider>
+    )
+}
